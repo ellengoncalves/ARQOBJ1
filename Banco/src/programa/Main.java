ea6b280780900e3a057a2d3dfb9fedd489bf571e
ellengoncalves <ellen.g@aluno.ifsp.edu.br>
2024-10-31 09:13:58 -0300

    tarefa - encapsulamento

--- Banco/src/programa/Main.java
+++ Banco/src/programa/Main.java
@@ -9,7 +9,6 @@ public class Main {
         Scanner input = new Scanner(System.in);
         ContaBancaria conta = new ContaBancaria();
 
-        // Cadastro da conta
         System.out.print("Digite o número da conta: ");
         int n_conta = input.nextInt();
         conta.setNumeroConta(n_conta);
@@ -25,23 +24,17 @@ public class Main {
         System.out.println("\nDados da conta cadastrada:");
         System.out.printf("Conta: %d%nTitular: %s%nSaldo: R$ %.2f%n", conta.getNumeroConta(), conta.getNomeTitular(), conta.getSaldo());
 
-        // Realizando um depósito
         System.out.print("\nDigite o valor do depósito: ");
         double valorDeposito = input.nextDouble();
         conta.depositar(valorDeposito);
         System.out.println("\nDados da conta após o depósito:");
         System.out.printf("Conta: %d%nTitular: %s%nSaldo: R$ %.2f%n", conta.getNumeroConta(), conta.getNomeTitular(), conta.getSaldo());
 
-        // Realizando um saque
         System.out.print("\nDigite o valor do saque: ");
         double valorSaque = input.nextDouble();
-        try {
-            conta.sacar(valorSaque);
-            System.out.println("\nDados da conta após o saque:");
-            System.out.printf("Conta: %d%nTitular: %s%nSaldo: R$ %.2f%n", conta.getNumeroConta(), conta.getNomeTitular(), conta.getSaldo());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        conta.sacar(valorSaque);
+        System.out.println("\nDados da conta após o saque:");
+        System.out.printf("Conta: %d%nTitular: %s%nSaldo: R$ %.2f%n", conta.getNumeroConta(), conta.getNomeTitular(), conta.getSaldo());
 
         input.close();
     }
